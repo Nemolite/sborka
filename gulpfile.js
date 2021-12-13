2cdd2de46b48c4be5cff3f0c4503dbb111553d2a
Nemolite <g25092011@mail.ru>
2021-12-13 11:37:17 +0300

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,86 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 const bSync = require('browser-sync').create();
+//const plus = require('gulp-concat');
+//const compress = require('gulp-uglify-es').default;
 
-const plus = require('gulp-concat');
-
-const compress = require('gulp-uglify-es').default;
-
-	// Определяем логику работы Browsersync
+	 /*
     function bsync() {
-        bSync.init({ // Инициализация Browsersync
-            server: { baseDir: 'C:/OpenServer/domains/sborka' }, // Указываем папку сервера
-            notify: true, // Отключаем уведомления
-            online: true // Режим работы: true или false
+        bSync.init({ 
+            server: { baseDir: './' }, 
+            notify: true, 
+            online: true 
         })
     }
-
     exports.bsync = bsync;
 
+   
 	function mainscripts() {
-        return src([ // Берем файлы из источников            
-            'js/main.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+        return src([           
+            'js/main.js', 
             ])
-        .pipe(plus('main.min.js')) // Конкатенируем в один файл
-        .pipe(compress()) // Сжимаем JavaScript
-        .pipe(dest('js/')) // Выгружаем готовый файл в папку назначения
-        .pipe(bSync.stream()) // Триггерим Browsersync для обновления страницы
+        .pipe(plus('main.min.js')) 
+        .pipe(compress()) 
+        .pipe(dest('js/')) 
+        .pipe(bSync.stream()) 
     }  
     
-    exports.goscript = mainscripts;
+    exports.mainscripts = mainscripts;
+    */ 
+       
+    function style() {        
+        return gulp.src('sass/main.sass')      
+        .pipe(sass().on('error',sass.logError))        
+        .pipe(gulp.dest('css'))        
+        .pipe(bSync.stream());
+    }
+    exports.style = style;
+    
+    function swatch() {
+        bSync.init({
+            server: {
+                baseDir: "./",
+                index: "/index.html"
+            }
+        });
+        gulp.watch('scss/main.sass', style);
+        gulp.watch('./index.html').on('change',bSync.reload);
+        // gulp.watch('./js/main.js').on('change', bSync.reload);
+    }
+    
+   
+    exports.watch = swatch;
+
+
+    /*Еще один вариант*/
+
+/*
+    const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+function style() {
+    return gulp.src('sass/main.sass')     
+    .pipe(sass().on('error',sass.logError))    
+    .pipe(gulp.dest('css'))    
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            index: "/index.html"
+        }
+    });
+    gulp.watch('sass/main.sass', style);
+    gulp.watch('./index.html').on('change',browserSync.reload);
+    gulp.watch('./js/main.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+*/
+
